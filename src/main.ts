fc18ab8eee369d16c51d3205cbbe00d392a9dd10
ashefor <mashefor@gmail.com>
2025-12-29 11:59:44 +0100

    feat: added swagger

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { GlobalExceptionFilter } from './common/filters/global-exception.filter'
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
 import { SanitizeHtmlPipe } from './common/pipes/sanitize-html.pipe';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,6 +20,17 @@ async function bootstrap() {
 
   // app.use(cookieParser());
   app.use(helmet());
+
+  const config = new DocumentBuilder()
+    .setTitle('Task Manager API')
+    .setDescription('The Task Manager API description')
+    .setVersion('1.0')
+    .addTag('tasks')
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
